0034d27618e18782c43e7dfa9d01ad055335f1ba
Sebastian Gruessinger <sgruessinger@inovex.de>
2020-03-26 20:48:08 +0100

    Basic chat App template added without ipfs.

---
+++ src/pages/ChatPage.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-page class="flex column">
+    <div class="q-pa-md column col justify-end">
+      <q-chat-message
+        v-for="message in messages"
+        :key="message.text"
+        :name="message.from"
+        :text="[message.text]"
+        :sent=" message.from == 'me'? true: false "
+      />
+
+    </div>
+    <q-footer elevated>
+      <q-toolbar>
+        <q-form
+          @submit="sendMessage"
+          class="full-width">
+          <q-input
+            v-model="newMessage"
+            bg-color="white"
+            outlined
+            rounded
+            label="Message"
+            dense>
+
+            <template v-slot:after>
+              <q-btn
+                @click="sendMessage"
+                round
+                dense
+                flat
+                type="submit"
+                color="white"
+                icon="send" />
+            </template>
+          </q-input>
+        </q-form>
+      </q-toolbar>
+    </q-footer>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+      return{
+          newMessage: '',
+          messages: [
+              {
+                  text: 'Hey du!',
+                  from: 'me'
+              },
+              {
+                  text: 'Hello, was geht bei dir',
+                  from: 'them'
+              },
+              {
+                  text: 'Danke gut und usw',
+                  from: 'me'
+              },
+              {
+                  text: 'Was sonst',
+                  from: 'me'
+              }
+          ]
+      }
+
+  },
+    methods: {
+      sendMessage() {
+          this.messages.push({
+              text: this.newMessage,
+              from: 'me'
+          })
+      }
+    }
+}
+</script>
---
+++ src/pages/PageUsers.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-page class="flex q-pa-md">
+    <q-list
+      class="full-width"
+      separator>
+      <q-item
+        v-for="user in users"
+        :key="user.id"
+        to="/chat"
+        clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge
+            :color="user.online ? 'light-green-5' : 'grey-4'">
+            {{ user.online ? 'Online': 'Offline' }}
+          </q-badge>
+
+        </q-item-section>
+      </q-item>
+
+
+    </q-list>
+
+  </q-page>
+</template>
+
+<script>
+
+
+
+
+    export default {
+        data () {
+            return {
+                 users: [ {
+                    id: 1,
+                    name: 'Peter',
+                    online:true
+                }, {
+                    id: 2,
+                    name: 'Mia',
+                     online: false
+                 }, {
+                    id: 3,
+                    name: 'Paul',
+                     online:true
+                 }]
+            }
+        }
+    }
+</script>
+
